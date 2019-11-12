fab13afb64c6f054428ea44943ce1f266bf07ba4
Lily <yyn605@outlook.com>
2019-11-11 22:42:04 -0800

    add more tests for BitInputStream & BitOutputStream

--- test/test_BitInputStream.cpp
+++ test/test_BitInputStream.cpp
@@ -18,3 +18,16 @@ TEST(BitInputStreamTests, SIMPLE_TEST) {
     ASSERT_EQ(1, bis.readBit());
     ASSERT_EQ(0, bis.readBit());
 }
+
+TEST(BitInputStreamTests, LONG_TEST) {
+    string bitsStr = "10101010";
+    string ascii = string(1, stoi(bitsStr, nullptr, 2));
+
+    stringstream ss;
+    ss.str(ascii + ascii);
+    BitInputStream bis(ss);
+
+    for (int i = 0; i < 15; i++) {
+        EXPECT_EQ((i + 1) % 2, bis.readBit());
+    }
+}
--- test/test_BitOutputStream.cpp
+++ test/test_BitOutputStream.cpp
@@ -11,9 +11,24 @@ TEST(BitOutputStreamTests, SIMPLE_TEST) {
     stringstream ss;
     BitOutputStream bos(ss);
     bos.writeBit(1);
+    bos.writeBit(0);
     bos.flush();
 
     string bitsStr = "10000000";
     unsigned int asciiVal = stoi(bitsStr, nullptr, 2);
+    ASSERT_EQ(ss.get(), asciiVal);
+}
+
+TEST(BitOutputStreamTests, LONG_TEST) {
+    stringstream ss;
+    BitOutputStream bos(ss);
+    for (int i = 0; i < 15; i++) {
+        bos.writeBit((i + 1) % 2);
+    }
+    bos.flush();
+
+    string bitsStr = "10101010";
+    unsigned int asciiVal = stoi(bitsStr, nullptr, 2);
+    ASSERT_EQ(ss.get(), asciiVal);
     ASSERT_EQ(ss.get(), asciiVal);
 }
